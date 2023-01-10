3f317f735982744c09e219a2e55da38c7bd3ce1f
daniel <blablabla@gmail.com>
2023-01-10 10:51:11 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+ 
+JAVASCRIPT/TYPESCRIPT
+
+*/
+console.log("Code Runner rodando com sucesso")
+
+// Calcular a porcentagem de um numero
+/*
+function porcentagem(numero,por)
+{
+    return (numero/100) * por;
+}
+
+    console.log(porcentagem(1000,  60));
+
+    */
+
+    //criar uma função que executa a soma de dois valores
+    /*
+
+    function soma(valor1, valor2){
+        return valor1 + valor2
+    }
+    console.log(soma(20.5, 30.5));
+    */
+
+    //criar uma função que exiba a soma, subtração, multiplicação e divisão entre dois números
+
+     /*
+    function operacoes (valor1, valor2) {
+        console.log(`A Soma entre ${valor1} e ${valor2} e ${valor1 + valor2}`);
+        console.log(`A Substração entre ${valor1} e ${valor2} e ${valor1 - valor2}`);
+        console.log(`A multiplicação entre ${valor1} e ${valor2} e ${valor1 * valor2}`);
+        console.log(`A Divisão entre ${valor1} e ${valor2} e ${valor1 / valor2}`);
+
+        
+    }
+    operacoes(50, 100);
+    */
+
+    //crie uma função que verifica se um numero inteiro e divisivel por tres e retorne como true ou false
+    //(true= verdadeiro)
+    //(false=falso)
+
+     /*
+    function divisivel(numeroint){
+        if (numeroint % 3 ==0){
+            return true
+        }else{
+            return false
+        }
+         divisivel(numeroint)
+    }
+    console.log(divisivel(9))
+    */
+
+    //criar um mecanismo que exiba se um usuário está logado ou não
+
+    /*
+    autenticado= false;
+    switch (autenticado) {
+        case true:
+        console.log("O Usuário está logado");
+        break;
+        case false:
+            console.log("O Usuário não estã logado");
+    }
+    */
+    //Fazer uma conversão de temperatura de Celsius pra fahrenheit
+    /*
+    function cpf(n){
+        return n * 1.8 + 32;
+    }
+    console.log(cpf(20))
+    */
+
+    //conversão de fahrenheit pra celsius
+
+    function fpc(n) {
+        return (n -32) / 1.8;
+    }
+    console.log(fpc(68));
+
+    
